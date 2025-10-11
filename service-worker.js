e1343d3a8582926c857428b4ea08b87752d390c6
shakif81 <38705392+shakif81@users.noreply.github.com>
2025-10-11 23:51:20 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,13 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('control-tiempos-v1').then(cache => {
-      return cache.addAll([
-        'index.html',
-        'manifest.json'
-      ]);
-    })
-  );
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open("v1").then(cache => cache.addAll([
+    "./",
+    "index.html",
+    "manifest.json",
+    "icon-192.png",
+    "icon-512.png"
+  ])));
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
-  );
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
 });
