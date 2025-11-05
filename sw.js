d464838438bde6b587a11d827ef3523d409f5f54
shakif81 <38705392+shakif81@users.noreply.github.com>
2025-11-05 20:12:48 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-// Service Worker mínimo pero funcional
-self.addEventListener('install', function(event) {
-  console.log('Service Worker instalado');
-});
-
-self.addEventListener('fetch', function(event) {
-  // Deja pasar todas las peticiones
-  return;
-});
